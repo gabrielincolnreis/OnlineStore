74809dcbd2e8b60abbe71d99bea12f0c71fc0063
Gabriellincoln2 <gabrielincoln17@gmail.com>
2021-01-14 01:29:50 -0300

    add migration

---
+++ src/modules/products/infra/typeorm/Product.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+   
+}
+
+export default Product;
